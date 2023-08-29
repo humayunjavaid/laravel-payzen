1b06af2fa163fefc8fc9aff15129d57dba796e30
humayunjavaid <humayunjavaid@users.noreply.github.com>
2023-08-29 07:16:17 +0000

    Fix styling

--- src/Payzen.php
+++ src/Payzen.php
@@ -5,10 +5,9 @@ declare(strict_types=1);
 namespace Humayunjavaid\Payzen;
 
 use Exception;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Http\Client\PendingRequest;
-
 
 /**
  *  Payzen
@@ -17,87 +16,68 @@ class Payzen
 {
     /**
      * Client Id provided by payzen
-     * @var string
      */
     protected string $clientId;
 
-
     /**
      * Client Secrect Key
-     * @var string
      */
     protected string $clientSecretKey;
 
-
     /**
      * Authentication Url
-     *
-     * @var string
      */
     protected string $authUrl;
 
-
     /**
      *  psidUrl
-     * @var string
      */
     protected string $psidUrl;
 
     /**
      * Token
-     * @var string
      */
     protected string $token;
 
-
     /**
      *  userData
-     * @var array
      */
     protected array $userData;
 
     /**
      * Account Number
-     * @var string
      */
     protected string $accountNumber;
 
     /**
      * Account Title
-     * @var string
      */
     protected string $accountTitle;
 
     /**
      * Amount
-     * @var string
      */
     protected string $amount;
 
     /**
      * Due Date
-     * @var string
      */
     protected string $dueDate;
 
     /**
      * Expiry Date
-     * @var string
      */
     protected string $expiryDate;
 
     /**
      * Challan Number
-     * @var string
      */
     protected string $challanNumber;
 
     /**
      * Service ID
-     * @var string
      */
     protected string $serviceId;
-
 
     public function __construct(
         string $clientId,
@@ -118,18 +98,13 @@ class Payzen
 
     }
 
-    /**
-     * @return Payzen
-     */
     public function psid(): Payzen
     {
         return new self();
     }
 
-
     /**
      * Generate Psid and return response
-     * @return Response
      */
     public function generate(): Response
     {
@@ -141,7 +116,7 @@ class Payzen
             'expiryDate' => $this->expiryDate ?? '',
             'amountWithinDueDate' => $this->amount,
             'amountAfterDueData' => $this->amount ?? '',
-            'amountBifurcation' => []
+            'amountBifurcation' => [],
         ];
 
         $request = $this->createRequest();
@@ -150,32 +125,30 @@ class Payzen
 
     }
 
-
     /**
      * Generate and retrieve the authentication token
-     * @return string
      */
     public function generateToken(): string
     {
         $response = Http::post($this->authUrl, [
             'clientId' => $this->clientId,
-            'clientSecretKey' => $this->clientSecretKey
+            'clientSecretKey' => $this->clientSecretKey,
         ])
             ->withHeaders([
                 'Content-type: application/vnd.api+json',
                 'Accept: application/vnd.api+json',
             ]);
 
-        if (!$response->successful()) {
-            throw new Exception("Failed to generate token");
+        if (! $response->successful()) {
+            throw new Exception('Failed to generate token');
         }
 
         $data = $response->json();
 
         $this->token = data_get($data, 'content.0.token.token', null);
 
-        if (!$this->token) {
-            throw new Exception("Token not received in the response");
+        if (! $this->token) {
+            throw new Exception('Token not received in the response');
         }
 
         return $this->token;
@@ -183,7 +156,6 @@ class Payzen
 
     /**
      * Build Http Request.
-     * @return PendingRequest
      */
     private function createRequest(): PendingRequest
     {
@@ -196,85 +168,59 @@ class Payzen
         return $request;
     }
 
-
-
-    /**
-     * @param array $userData
-     * @return self
-     */
     public function setUserData(array $userData): self
     {
         $this->userData = $userData;
+
         return $this;
     }
 
-    /**
-     * @param string $accountNumber
-     * @return self
-     */
     public function setAccountNumber(string $accountNumber): self
     {
         $this->accountNumber = $accountNumber;
+
         return $this;
     }
 
-    /**
-     * @param string $accountTitle
-     * @return self
-     */
     public function setAccountTitle(string $accountTitle): self
     {
         $this->accountTitle = $accountTitle;
+
         return $this;
     }
 
-    /**
-     * @param string $amount
-     * @return self
-     */
     public function setAmount(string $amount): self
     {
         $this->amount = $amount;
+
         return $this;
     }
 
-    /**
-     * @param string $dueDate
-     * @return self
-     */
     public function setDueDate(string $dueDate): self
     {
         $this->dueDate = $dueDate;
+
         return $this;
     }
 
-    /**
-     * @param string $expiryDate
-     * @return self
-     */
     public function setExpiryDate(string $expiryDate): self
     {
         $this->expiryDate = $expiryDate;
+
         return $this;
     }
 
-    /**
-     * @param string $challanNumber
-     * @return self
-     */
     public function setChallanNumber(string $challanNumber): self
     {
         $this->challanNumber = $challanNumber;
+
         return $this;
     }
 
-    /**
-     * @param string $serviceId
-     * @return self
-     */
     public function setServiceId(string $serviceId): self
     {
         $this->serviceId = $serviceId;
+
         return $this;
     }
 }
